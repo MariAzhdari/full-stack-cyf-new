08a63058ca818e659c57049dd3577cbf9b1e9bbd
Mari Azh <Marzi.aghchay@gmail.com>
2023-07-04 10:31:08 +0100

    start client

--- server/index.js
+++ server/index.js
@@ -79,6 +79,6 @@ app.delete("/todo/:id", async (req, res) => {
 });
  
 
-app.listen(5000, () => {
-  console.log("server has started on port 5000");
+app.listen(5002, () => {
+  console.log("server has started on port 5002");
 });
